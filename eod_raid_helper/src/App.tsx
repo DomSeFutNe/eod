4801a0820e8ecbb22850db72a7a9ef604e62972b
dominic.seel <dominic.seel@futurenext.de>
2025-03-09 11:57:53 +0100

    Füge Prettier- und ESLint-Konfigurationen hinzu; aktualisiere App-Komponente zur Verwendung von Providers

--- eod_raid_helper/src/App.tsx
+++ eod_raid_helper/src/App.tsx
@@ -1,21 +1,9 @@
-import reactLogo from './assets/react.svg';
-
-import viteLogo from '/vite.svg';
-
 import './App.css';
-import Providers from './components/context/Providers';
 
 function App() {
   return (
-    <Providers>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+    <>
+      <div></div>
       <h1>Vite + React</h1>
       <div className="card">
         <p>
@@ -23,7 +11,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </Providers>
+    </>
   );
 }
 
